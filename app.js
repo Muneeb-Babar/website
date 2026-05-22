0982b4b799970b99226375ecfcaf2fbc4a4510c5
Muneeb-Babar <muneeb63ahmad63@gmail.com>
2026-05-22 00:08:42 -0700

    add website

---
+++ app.js
@@ -0,0 +1,23 @@
+
+
+// function sum(){
+//     a=12
+//     b=20
+//     console.log("add", a+b)
+// }
+
+// sum()
+
+// function sum(){
+//     a=10
+//     b=15
+//     return a+b
+// }
+
+// console.log(sum())
+
+function sum(a,b){
+    console.log(a+b)
+}
+
+sum(50,20)
